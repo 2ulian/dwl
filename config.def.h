a25d002a6e7ed5135fa839561b61538a7c921955
Julian <rayconstantyjulian@gmail.com>
2025-12-13 16:16:20 +0100

    modify cursor size

--- config.def.h
+++ config.def.h
@@ -22,9 +22,10 @@ static const float rootcolor[] = COLOR(0x000000ff);
  * behavior */
 static const float fullscreen_bg[] = {0.0f, 0.0f, 0.0f,
                                       1.0f}; /* You can also use glsl colors */
-static const char *cursor_theme = NULL;
+static const char *cursor_theme =
+    macOS; /* set to NULL to use default cursor theme */
 static const char cursor_size[] =
-    "32"; /* Make sure it's a valid integer, otherwise things will break */
+    "64"; /* Make sure it's a valid integer, otherwise things will break */
 static int enableautoswallow =
     1; /* enables autoswallowing newly spawned clients */
 static float swallowborder = 1.0f; /* add this multiplied by borderpx to border
