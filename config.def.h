7f079a2b12cc03790857bc4db1878f7edfabcb21
Julian <rayconstantyjulian@gmail.com>
2025-12-13 17:02:11 +0100

    bold font

--- config.def.h
+++ config.def.h
@@ -16,7 +16,7 @@ static const unsigned int systrayspacing = 2; /* systray spacing */
 static const int showsystray = 1;             /* 0 means no systray */
 static const int showbar = 1;                 /* 0 means no bar */
 static const int topbar = 1;                  /* 0 means bottom bar */
-static const char *fonts[] = {"terminus:size=10"};
+static const char *fonts[] = {"terminus:bold:size=10"};
 static const float rootcolor[] = COLOR(0x000000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old
  * behavior */
