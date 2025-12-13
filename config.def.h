9414d482b61042e6e74d15dea81e4584ce3c0156
Julian <rayconstantyjulian@gmail.com>
2025-12-13 14:50:23 +0100

    add dmenucmd

--- config.def.h
+++ config.def.h
@@ -147,6 +147,7 @@ static const enum libinput_config_tap_button_map button_map =
 /* commands */
 static const char *termcmd[] = {"kitty", NULL};
 static const char *menucmd[] = {"rofi", "-show", "drun", NULL};
+static const char *dmenucmd[] = {"wmenu", NULL};
 
 static const Key keys[] = {
     /* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
